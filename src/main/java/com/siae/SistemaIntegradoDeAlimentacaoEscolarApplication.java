8bec57bdc56bce7e782427951807208bc3ca2ffc
cavalcantgus <marcelo.cavalcanti.sousa@gmail.com>
2024-11-30 14:44:51 -0300

    Adicionando projeto Spring

---
+++ src/main/java/com/siae/SistemaIntegradoDeAlimentacaoEscolarApplication.java
@@ -0,0 +1,13 @@
+package com.siae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaIntegradoDeAlimentacaoEscolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaIntegradoDeAlimentacaoEscolarApplication.class, args);
+	}
+
+}

0f1a49e3292badffa45e6fbe1e80457f03721353
cavalcantgus <marcelo.cavalcanti.sousa@gmail.com>
2024-11-30 18:44:57 -0300

    Ajustando GET de UserController

--- src/main/java/com/siae/controllers/UserController.java
+++ src/main/java/com/siae/controllers/UserController.java
@@ -31,7 +31,7 @@ public class UserController {
 	}
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<User> findById(Long id) {
+	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User user = service.findById(id);
 		return ResponseEntity.ok().body(user);
 	}

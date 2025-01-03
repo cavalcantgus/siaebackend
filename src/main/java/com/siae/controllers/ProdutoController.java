439a90748c647ee0f5415d30e0f5d568db6020ed
cavalcantgus <marcelo.cavalcanti.sousa@gmail.com>
2025-01-03 18:29:25 -0300

    Implementando Entitade, Repository, Service e Controller de Produto

---
+++ src/main/java/com/siae/controllers/ProdutoController.java
@@ -0,0 +1,49 @@
+package com.siae.controllers;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.siae.entities.Produto;
+import com.siae.services.ProdutoService;
+
+@RestController
+@RequestMapping("/public/produtos")
+public class ProdutoController {
+	
+	@Autowired
+	private ProdutoService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Produto>> findAll() {
+		List<Produto> produtos = service.findAll();
+		return ResponseEntity.ok().body(produtos);
+	}
+	
+	// GetMapping("/{id}")
+	
+	@PostMapping("/produto") 
+	public ResponseEntity<Produto> insert(@RequestBody Produto obj) {
+		Produto produto = service.insert(obj);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(produto.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(produto);
+	}
+	
+	@PutMapping("/produto/{id}") 
+	public ResponseEntity<Produto> update(@PathVariable Long id, @RequestBody Produto obj) {
+		Produto produto = service.update(id, obj);
+		return ResponseEntity.ok().body(produto);
+	}
+}
---
+++ src/main/java/com/siae/services/ProdutoService.java
@@ -0,0 +1,51 @@
+package com.siae.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.siae.entities.Produto;
+import com.siae.repositories.ProdutoRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class ProdutoService {
+	
+	@Autowired
+	private ProdutoRepository repository;
+	
+	public List<Produto> findAll() {
+		return repository.findAll();
+	}
+	
+	public Produto findById(Long id) {
+		Optional<Produto> produto = repository.findById(id);
+		return produto.orElseThrow(() -> new EntityNotFoundException("Produto não encontrado"));
+	}
+	
+	public Produto insert(Produto produto) {
+		return repository.save(produto);
+	}
+	
+	public Produto update(Long id, Produto produto) {
+		try { 
+			Produto produtoTarget = repository.findById(id)
+					.orElseThrow(() -> new EntityNotFoundException("Produto não encontrado"));
+			updateData(produto, produtoTarget);
+			return repository.save(produtoTarget);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	private void updateData(Produto produto, Produto produtoTarget) {
+		produtoTarget.setDescricao(produto.getDescricao());
+		produtoTarget.setUnidade(produto.getUnidade());
+		produtoTarget.setPrecoMedio(produto.getPrecoMedio());
+	}
+}

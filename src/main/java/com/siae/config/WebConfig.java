44239bb3441533b9971e84e6713628d54ae047df
cavalcantgus <marcelo.cavalcanti.sousa@gmail.com>
2025-01-10 17:02:51 -0300

    Alterando endpoint de acesso ao back

--- src/main/java/com/siae/config/WebConfig.java
+++ src/main/java/com/siae/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Permitir todas as rotas
-                .allowedOrigins("http://localhost:5173")  // Permitir o front-end
+                .allowedOrigins("*")  // Permitir o front-end
                 .exposedHeaders("Authorization")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")  // Métodos permitidos
                 .allowedHeaders("*");  // Cabeçalhos permitidos

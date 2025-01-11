75a7806a19b8bc8dac59fc5cff9d1463c0c71506
cavalcantgus <marcelo.cavalcanti.sousa@gmail.com>
2025-01-11 08:37:18 -0300

    Alterando campo descricao para unique

--- src/main/java/com/siae/entities/Produto.java
+++ src/main/java/com/siae/entities/Produto.java
@@ -2,6 +2,7 @@ package com.siae.entities;
 
 import java.math.BigDecimal;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,6 +23,7 @@ public class Produto {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column(unique = true, nullable = false)
 	private String descricao;
 	private String especificacao;
 	private String unidade;

6a7d19c577bfb4aea9701e58e9312aedfae1ccfd
Marco <marcofreires86@gmail.com>
2025-01-27 15:26:50 -0300

    DTO de Entrega

---
+++ src/main/java/com/siae/dto/EntregaDTO.java
@@ -0,0 +1,19 @@
+package com.siae.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class EntregaDTO {
+    private Long produtorId;
+    private LocalDate dataEntrega;
+    private List<Long> produtoIds;
+    private BigDecimal quantidade;
+}

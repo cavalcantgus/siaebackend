32cb7aa5dc484c5625ad44b2ba80709579fb96bb
cavalcantgus <marcelo.cavalcanti.sousa@gmail.com>
2025-03-22 14:29:17 -0300

    Ata Da Chamada PDF

---
+++ src/main/java/com/siae/repositories/AtaRepository.java
@@ -0,0 +1,7 @@
+package com.siae.repositories;
+
+import com.siae.entities.Ata;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AtaRepository extends JpaRepository<Ata, Long> {
+}
